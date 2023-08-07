7542bdfcc44c638dfdfb55a62daa560c9d5b2e37
Toasty <68469450+HeatedBread@users.noreply.github.com>
2023-08-06 19:15:09 -0500

    feat(navbar mobile): implement mobile navbar

---
+++ src/components/NavbarMobile.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import "./NavbarMobile.css"
+import { AiOutlineHome } from "react-icons/ai"
+import { AiOutlineUser} from "react-icons/ai"
+import { MdWork } from "react-icons/md"
+import { BsFillChatLeftDotsFill } from "react-icons/bs"
+import { useState } from "react"
+import { Link } from 'react-router-dom'
+
+const NavbarMobile = () => {
+    const [activeNav, setActiveNav] = useState('/');
+
+    return (
+        <nav>
+            <Link to="/" onClick={() => setActiveNav('/')} className={activeNav === "/" ? 'active' : ''}><AiOutlineHome/></Link>
+            <Link to="/about" onClick={() => setActiveNav('/about')} className={activeNav === "/about" ? 'active' : ''}><AiOutlineUser/></Link>
+            <Link to="/projects" onClick={() => setActiveNav('/projects')} className={activeNav === "/projects" ? 'active' : ''}><MdWork/></Link>
+            <Link to="/contact" onClick={() => setActiveNav('/contact')} className={activeNav === "/contact" ? 'active' : ''}><BsFillChatLeftDotsFill/></Link>
+        </nav>
+    )
+}
+
+export default NavbarMobile;
